177ba846b5f6a24e339bd74c55b621ea25b7e99b
GabrielSuoye <suoyegabriel@gmail.com>
2026-06-06 14:11:02 +0100

    books app v1 completed

--- FastApi/books.py
+++ FastApi/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 
 app = FastAPI()
@@ -23,3 +23,66 @@ async def get_book(book_title: str):
     for book in BOOKS:
         if book.get("title").casefold() == book_title.casefold():
             return book
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/{book_author}")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if (
+            book.get("author").casefold() == book_author.casefold()
+            and book.get("category").casefold() == category.casefold()
+        ):
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.post("/books/add_book")
+async def add_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == updated_book.get("title").casefold():
+            BOOKS[i] = updated_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_author: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
+
+
+"""
+    Assignment
+
+Here is your opportunity to keep learning!
+
+1. Create a new API Endpoint that can fetch all books from a specific author 
+    using either Path Parameters or Query Parameters.
+
+"""
+
+
+@app.get(
+    "/books/byauthor/{author}"
+)  # Remove or change {author} to make it a query. Also move it up so it doesn't interfere with previous query endpoint.
+async def get_book_by_author(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("author").casefold() == author.casefold():
+            books_to_return.append(book)
+    return books_to_return
