f047b30bdc71b3807b5ebeac2663224dbdcba900
GabrielSuoye <suoyegabriel@gmail.com>
2026-09-23 01:01:01 +0100

    Implemented authorization and authenthication using bcrypt, OAuth2, and JWT.

---
+++ FastApi/TodoApp/routers/auth.py
@@ -0,0 +1,125 @@
+from datetime import timedelta, timezone, datetime
+from typing import Annotated, cast
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic.main import BaseModel
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
+import jwt
+
+SECRET_KEY = "d5efbaafe996260669e6e99a84afd2742c2298ebf482d592b73cf5fd36400c1d"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+# Hashing Helpers
+def hash_password(password: str) -> str:
+    if len(password.encode("utf-8")) > 72:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Password is too long for bcrypt setup.",
+        )
+    return pwd_context.hash(password)
+
+
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+
+    if not pwd_context.verify(password, user.hashed_password):
+        return False
+
+    return user
+
+
+# JWT Helpers
+def create_access_token(data: dict, expires_delta: timedelta | None):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        user_id = cast(int, payload.get("id"))
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
+            )
+        return {"username": username, "id": user_id}
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Ivallid token"
+        )
+
+
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        hashed_password=pwd_context.hash(create_user_request.password),
+        role=create_user_request.role,
+        is_active=True,
+    )
+
+    db.add(create_user_model)
+    db.commit()
+    return create_user_model
+
+
+@router.post("/token", response_model=Token)
+async def login(
+    db: db_dependency, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or password.",
+        )
+
+    token = create_access_token(
+        data={"sub": user.username},
+        expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+    )
+    return {"access_token": token, "token_type": "bearer"}
